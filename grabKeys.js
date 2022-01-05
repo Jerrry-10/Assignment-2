e7101fb55cb71d9cfd25a3a530ab0f64bf10df30
Jerry <78663777+Jerrry-10@users.noreply.github.com>
2022-01-05 17:19:25 -0500

    grabkeys

---
+++ grabKeys.js
@@ -0,0 +1,13 @@
+const object1 = {
+    a: 'somestring',
+    b: 42,
+    c: false
+  };
+  function grabKeys(obj){
+    let arr = []
+    for(key in obj){
+        arr.push(key)
+    }
+    return arr;
+  }
+  console.log(grabKeys(object1))
